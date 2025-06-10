6447690eddfbcf411d93424e9aca04034c02d2b7
NiKhilForCybersec <nikhileswarreddy.c@gmail.com>
2025-06-10 09:24:07 -0400

    Add multi-file analysis support

--- src/SecuNik.Core/Interfaces/IAnalysisEngine.cs
+++ src/SecuNik.Core/Interfaces/IAnalysisEngine.cs
@@ -7,6 +7,7 @@ namespace SecuNik.Core.Interfaces
     public interface IAnalysisEngine
     {
         Task<AnalysisResult> AnalyzeFileAsync(AnalysisRequest request);
+        Task<AnalysisResult> AnalyzeFilesAsync(IEnumerable<AnalysisRequest> files);
         Task<List<string>> GetSupportedFileTypesAsync();
         Task<bool> CanProcessFileAsync(string filePath);
     }
---
+++ tests/SecuNik.API.Tests/MultiUploadTests.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using SecuNik.API.Controllers;
+using SecuNik.Core.Interfaces;
+using SecuNik.Core.Models;
+using Xunit;
+
+public class MultiUploadTests
+{
+    [Fact]
+    public async Task UploadMultiple_CallsEngine()
+    {
+        var mockEngine = new Mock<IAnalysisEngine>();
+        mockEngine.Setup(e => e.CanProcessFileAsync(It.IsAny<string>())).ReturnsAsync(true);
+        mockEngine.Setup(e => e.AnalyzeFilesAsync(It.IsAny<IEnumerable<AnalysisRequest>>()))
+            .ReturnsAsync(new AnalysisResult { Technical = new TechnicalFindings() });
+        mockEngine.Setup(e => e.GetSupportedFileTypesAsync()).ReturnsAsync(new List<string> { ".log" });
+
+        var config = new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string,string>()).Build();
+        var controller = new AnalysisController(mockEngine.Object, new NullLogger<AnalysisController>(), config);
+
+        var ms = new MemoryStream(Encoding.UTF8.GetBytes("test"));
+        var file = new FormFile(ms, 0, ms.Length, "file", "test.log");
+        var files = new FormFileCollection { file, file };
+
+        var result = await controller.UploadMultiple(files, null, new AnalysisOptionsDto());
+
+        result.Result.Should().BeOfType<OkObjectResult>();
+        mockEngine.Verify(e => e.AnalyzeFilesAsync(It.IsAny<IEnumerable<AnalysisRequest>>()), Times.Once());
+    }
+}
